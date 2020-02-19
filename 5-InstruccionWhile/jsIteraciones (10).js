301dba95c9f36ed9f0524e8e04c0b3fea727fd61
alumno <alumno@utnfra.com.ar>
2020-02-19 12:24:23 -0300

    clase 5 v.2

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -17,48 +17,55 @@ function mostrar()
 	
 
 
+
+
+do {
 	do{
 	    var num = prompt("ingrese un numero.");
 	    num= parseInt;
-    }while (isNaN(num));
+		
+	}while (isNaN(num));
 	    
-		if (num>0) {
+	if (num>0) {
 				sumapos+=num;
 				contpos++;
 				
-	    }else if (num < 0) {
+	} else if (num < 0) {
 				sumaneg += num;
 				contneg++;
 				
-	    }else{
+	} else {
 			    contceros++;
-		}
-        if (num % 2 == 0) {
+	}
+    if (num % 2 == 0) {
 			cantpares++;
-		}
-		respuesta = confirm("desea continuar");
-    
-		prompos = sumapos / contpos;
+	}
+respuesta = confirm("desea continuar");
 		
-		promneg = sumaneg / contneg;
+	
+} while (respuesta);
 
-		diferencia =sumapos - sumaneg;
+prompos = sumapos / contpos;
+
+promneg = sumaneg / contneg;
+
+diferencia =sumapos - sumaneg;
 		
-		document.write("promedio de posiivos " + prompos + "<br >");
-
-		document.write("promedio de negativos " + promneg + "<br >");
-
-		document.write("diferencia " + diferencia + "<br >");
-
-		document.write("suma de posiivos " + sumapos + "<br >");
-		
-		document.write("suma de negativos " + sumaneg + "<br >");
-
-		document.write("cantidad de positivos " + contpos + "<br >");
-
-		document.write("cantidad de nnegativos " + contneg + "<br >");
-
-		document.write("cantidad de ceros " + contceros + "<br >");
-
-		document.write("cantidad de pares " + cantpares + "<br >");
+document.write("promedio de posiivos " + prompos + "<br >");
+	
+document.write("promedio de negativos " + promneg + "<br >");
+	
+document.write("diferencia " + diferencia + "<br >");
+	
+document.write("suma de posiivos " + sumapos + "<br >");
+			
+document.write("suma de negativos " + sumaneg + "<br >");
+	
+document.write("cantidad de positivos " + contpos + "<br >");
+	
+document.write("cantidad de nnegativos " + contneg + "<br >");
+	
+document.write("cantidad de ceros " + contceros + "<br >");
+	
+document.write("cantidad de pares " + cantpares + "<br >");
 }	//FIN DE LA FUNCIÓN
